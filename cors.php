a0800771596e3a54795d7d042f414c14799703f0
S-Mlangeni <smlangeni21@gmail.com>
2021-12-12 20:45:00 +0200

    update

--- cors.php
+++ cors.php
@@ -1,5 +1,5 @@
 <?php
-    header('Access-Control-Allow-Origin: https://ordering-system-client.herokuapp.com');
+    header('Access-Control-Allow-Origin: https://stock-management-system-client.herokuapp.com');
     header('Access-Control-Allow-Methods: GET, POST');
     header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization");
 ?>
